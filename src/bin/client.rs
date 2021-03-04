60c53e1f9c5801dd359d033dc90571b838e68b72
Ricardo Delfin <me@rdelfin.com>
2021-03-04 16:30:27 +0000

    Flipped client and server responsibilities

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,12 +1,9 @@
 use ibverbs::EndpointMsg;
-use std::{error, fs, net::TcpStream, path::PathBuf};
+use std::{error, net::TcpStream};
 
 const WR_ID: u64 = 9_926_239_128_092_127_829;
 
 fn main() -> Result<(), Box<dyn error::Error>> {
-    let images = load_images(PathBuf::from("data"), "RGB8")?;
-    let bytes_per_image = images[0].len();
-
     let devices = ibverbs::devices().unwrap();
     let device = devices.iter().next().expect("no rdma device available");
     println!(
@@ -19,7 +16,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     let dev_attr = ctx.clone().query_device().unwrap();
     let pd = ctx.clone().alloc_pd().unwrap();
     let cq = ctx.create_cq(dev_attr.max_cqe, 0).unwrap();
-    let mut mr = pd.allocate::<u8>(bytes_per_image).unwrap();
+    let mut mr = pd.allocate::<u8>(7864320 + 1024 * 1024).unwrap();
     let laddr = ibverbs::RemoteAddr((&mr[0..]).as_ptr() as u64);
     let lkey = mr.rkey();
 
@@ -46,56 +43,16 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         let rmsg: EndpointMsg = bincode::deserialize_from(&mut stream).unwrap();
         rmsg
     };
-    let rkey = rmsg.rkey;
-    let raddr = rmsg.raddr;
+    // let rkey = rmsg.rkey;
+    // let raddr = rmsg.raddr;
 
     let mut qp = qp_init.handshake(rmsg.into()).unwrap();
 
-    println!("Copying image...");
-    for i in 0..bytes_per_image {
-        mr[i] = images[0][i];
-    }
-    println!("Done!");
-
-    let mut completions = [ibverbs::ibv_wc::default()];
-
     // Write
     unsafe {
-        qp.post_send(&mut mr, 0..bytes_per_image, WR_ID)?;
+        qp.post_receive(&mut mr, 0..7864320, WR_ID)?;
     }
-    loop {
-        let completed = cq
-            .poll(&mut completions)
-            .expect("ERROR: Could not poll CQ.");
-        if completed.is_empty() {
-            continue;
-        }
-        if completed.iter().any(|wc| wc.wr_id() == WR_ID) {
-            break;
-        }
-    }
+    println!("Data at mr[0] = {}", mr[0]);
 
     Ok(())
-}
-
-fn load_images(dir: PathBuf, ext: &str) -> Result<Vec<Vec<u8>>, Box<dyn error::Error>> {
-    Ok(fs::read_dir(dir.as_path())?
-        .filter(|e| match e {
-            Ok(entry) => {
-                let is_file = {
-                    match entry.file_type() {
-                        Ok(ftype) => ftype.is_file(),
-                        Err(_) => false,
-                    }
-                };
-                let correct_ext = { entry.path().extension().unwrap() == ext };
-                is_file && correct_ext
-            }
-            Err(_) => false,
-        })
-        .map(|e| fs::read(e.unwrap().path().as_path()))
-        .collect::<Result<Vec<_>, _>>()?
-        .iter()
-        .map(|data| data[32..].into())
-        .collect())
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,9 +1,12 @@
 use ibverbs::EndpointMsg;
-use std::{env, error, net::TcpListener};
+use std::{env, error, fs, net::TcpListener, path::PathBuf};
 
 const WR_ID: u64 = 9_926_239_128_092_127_829;
 
 fn main() -> Result<(), Box<dyn error::Error>> {
+    let images = load_images(PathBuf::from("data"), "RGB8")?;
+    let bytes_per_image = images[0].len();
+
     let devices = ibverbs::devices().unwrap();
     let device = devices.iter().next().expect("no rdma device available");
     println!(
@@ -18,7 +21,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     let pd = ctx.clone().alloc_pd().unwrap();
     let cq = ctx.create_cq(dev_attr.max_cqe, 0).unwrap();
 
-    let mut mr = pd.allocate::<u64>(7864352 + 1024 * 1024).unwrap();
+    let mut mr = pd.allocate::<u8>(bytes_per_image).unwrap();
     let laddr = (&mr[0..]).as_ptr() as u64;
 
     let qp_init = pd
@@ -52,13 +55,53 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     let mut qp = qp_init
         .handshake(rendpoint)
         .unwrap_or_else(|e| panic!("ERROR: failed to handshake: {}", e));
+    let mut completions = [ibverbs::ibv_wc::default()];
 
-    println!("RDMA handshake successfull");
+    println!("Copying image...");
+    for i in 0..bytes_per_image {
+        mr[i] = images[0][i];
+    }
+    println!("Done!");
+
+    // println!("RDMA handshake successful");
     unsafe {
-        qp.post_receive(&mut mr, 0..7864320, WR_ID)?;
+        qp.post_send(&mut mr, 0..bytes_per_image, WR_ID)?;
+    }
+    loop {
+        let completed = cq
+            .poll(&mut completions)
+            .expect("ERROR: Could not poll CQ.");
+        if completed.is_empty() {
+            continue;
+        }
+        if completed.iter().any(|wc| wc.wr_id() == WR_ID) {
+            break;
+        }
     }
 
     println!("Someone has written to the memory region, got: {}", mr[0]);
 
     Ok(())
+}
+
+fn load_images(dir: PathBuf, ext: &str) -> Result<Vec<Vec<u8>>, Box<dyn error::Error>> {
+    Ok(fs::read_dir(dir.as_path())?
+        .filter(|e| match e {
+            Ok(entry) => {
+                let is_file = {
+                    match entry.file_type() {
+                        Ok(ftype) => ftype.is_file(),
+                        Err(_) => false,
+                    }
+                };
+                let correct_ext = { entry.path().extension().unwrap() == ext };
+                is_file && correct_ext
+            }
+            Err(_) => false,
+        })
+        .map(|e| fs::read(e.unwrap().path().as_path()))
+        .collect::<Result<Vec<_>, _>>()?
+        .iter()
+        .map(|data| data[32..].into())
+        .collect())
 }
